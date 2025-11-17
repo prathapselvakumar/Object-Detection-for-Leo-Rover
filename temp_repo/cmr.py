1bc9e34447d95f89a6f5c356c0e7bb22f686b169
PRATHAP <PRATHAPSK@HOTMAIL.COM>
2025-11-17 20:39:53 +0000

    Removed temp files

--- temp_repo/cmr.py
+++
@@ -1,8 +0,0 @@
-from pygrabber.dshow_graph import FilterGraph
-
-graph = FilterGraph()
-devices = graph.get_input_devices()
-
-print("🎥 Connected Cameras:")
-for i, name in enumerate(devices):
-    print(f"{i}: {name}")
